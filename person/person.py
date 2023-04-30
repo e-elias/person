ad4c2b5025c682208ef1ddd61911815873795190
e-elias <89999115+e-elias@users.noreply.github.com>
2023-04-30 16:27:17 -0400

    Update person.py

--- person/person.py
+++ person/person.py
@@ -6,7 +6,8 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import numpy as np
-
+from geometry_msgs.msg import Twist
+from rclpy.qos import qos_profile_sensor_data, qos_profile_system_default
 
 class Camera(Node):
 	
@@ -16,6 +17,16 @@ class Camera(Node):
 		self.subscription = self.create_subscription(Image, '/color/preview/image', self.listener_callback, 10)
 		self.subscription
 		self.br = CvBridge()
+		
+		self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', qos_profile_system_default)
+		
+	def drive(self, linear_x, angular_z):
+		msg = Twist()
+		msg.angular.z = angular_z
+		msg.linear.x = linear_x
+		self.cmd_vel_pub.publish(msg)
+		print(angular_z)
+		print(linear_x)
 		
 	def listener_callback(self, data):
 		#self.get_logger().info('Receiving video frame')
@@ -27,6 +38,11 @@ class Camera(Node):
 		boxes = np.array([[x, y, x+w, y+h] for (x, y, w, h) in boxes])
 		for (xA, yA, xB, yB) in boxes:
 			self.get_logger().info('Detecting Person')
+			x_c, y_c = (xA + w/2), (yA + h/2)
+			offset_x = x_c - 2104
+			theta = offset_x / 4208
+			angular = - 2 * theta
+			drive(0.5, angular)
 			#cv2.rectangle(imCV, (xA, yA), (xB, yB),
 			#	(0, 255, 0), 2)
 			#x_c, y_c = (xA + w/2), (yA + h/2)
@@ -41,7 +57,9 @@ class Camera(Node):
 		#current_frame = self.br.imgmsg_to_cv2(data)
 		#cv2.imshow("camera", current_frame)
 		#cv2.waitKey(1)
-		
+	
+	
+	
 	def ImageProcessor(self, imCV):
 		print("HERE")
 		hog = cv2.HOGDescriptor()
