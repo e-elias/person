9279b434c6261c380da98a66e8a9dec2ac67eadf
e-elias <89999115+e-elias@users.noreply.github.com>
2023-05-03 01:12:08 -0400

    Update person.py

--- person/person.py
+++ person/person.py
@@ -95,7 +95,7 @@ class Camera(Node):
 				
 				#(xA, yA, xB, yB) = corners
 				#x_c, y_c = (xA + xB/2), (yA + yB/2)
-				widthBounding = int(bottomRight[0] - topLeft[0])
+				#widthBounding = int(bottomRight[0] - topLeft[0])
 				#print("WIDTH OF BOUNDING BOX")
 				#print(widthBounding)
 				#if widthBounding >= 58:
